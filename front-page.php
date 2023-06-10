57788ba41c7d101cedaca4e053abf454368d7459
alisa corp <amg.lv@protonmail.com>
2023-06-10 02:03:53 +0200

    fix(front-page): add sidebar, template for no-content

--- front-page.php
+++ front-page.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
-    <div class="ui grid">
-		<?php
-		if ( have_posts() ) : ?>
+    <!-- Start main container -->
+    <div class="ui main text container">
+        <div class="ui grid">
             <div class="thirteen wide column custom-x-grid-padding-table-posts">
+				<?php
+				if ( have_posts() ) { ?>
                 <table class="ui table striped black inverted very compact border-radius-0 border-1 selectable celled unstackable">
                     <thead>
                     <tr>
@@ -17,50 +19,16 @@
 					while ( have_posts() ) {
 						the_post();
 						get_template_part( 'template-parts/content', 'post' );
+					}
+
+					} else {
+						get_template_part( 'template-parts/no', 'content' );
 					} ?>
                     </tbody>
                 </table>
             </div>
-		<?php endif; ?>
-        <div class="five column custom-x-grid-padding-side-menu">
-            <div class="ui vertical menu border-radius-0 border-1 inverted">
-                <div class="item">
-                    <div class="header">OS</div>
-                    <div class="menu">
-                        <a class="item">Linux</a>
-                        <a class="item">MacOS</a>
-                        <a class="item">Windows</a>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="header">CMS</div>
-                    <div class="menu">
-                        <a class="item">Wordpress</a>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="header">Community dashboard</div>
-                    <div class="menu">
-                        <a class="item">IPB</a>
-                        <a class="item">phpBB</a>
-                        <a class="item">SMF</a>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="header">Hosting</div>
-                    <div class="menu">
-                        <a class="item">VPS</a>
-                        <a class="item">VDS</a>
-                        <a class="item">Dedicated</a>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="header">Support</div>
-                    <div class="menu">
-                        <a class="item">E-mail Support</a>
-                        <a class="item">FAQs</a>
-                    </div>
-                </div>
+            <div class="three wide column custom-x-grid-padding-side-menu">
+				<?php kdb_side_menu_render(); ?>
             </div>
         </div>
     </div>
